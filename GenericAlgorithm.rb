19764654fd86c09c7c2080ee0024e27c26e29b87
fr-matsuo <matsuo@fact-real.com>
2014-07-18 03:08:26 +0000

    評価の呼び出しを追加

--- GenericAlgorithm.rb
+++ GenericAlgorithm.rb
@@ -4,4 +4,10 @@ class GenericAlgorithm
     def initialize(genoms)
         @genoms = genoms
     end
+
+    def evaluate
+        @genoms.each{|genom|
+            genom.evaluate
+        }
+    end
 end
---
+++ GenomBase.rb
@@ -0,0 +1,7 @@
+class GenomBase
+    include Comparable
+
+    def evaluate
+        @evaluate = rand(100);
+    end
+end
